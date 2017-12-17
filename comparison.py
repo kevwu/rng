e4b8582aabd74421b11cfbdf50f07eee7ef12a68
Kevin Wu <kevin@kevinywu.com>
2017-12-17 12:39:10 -0600

    Add titles to graphs

--- comparison.py
+++ comparison.py
@@ -1,51 +1,55 @@
 import time
 
-import rng
+import rng # custom RNG, rng.py
 import math
-import random
+import random # for Python's MT PRNG
 
 import os # for urandom
 
 import matplotlib.pyplot as plt
-
 import numpy as np
+import scipy.special
 
 IMG_DIR = 'img/'
 
 def test_rng(name, samples, gen):
+	print("Testing: " + name)
+	# generate values until we reach the desired number of samples
 	values = list()
 	while(len(values) < samples):
 		values += gen()
-		print(values)
-		print(len(values))
+		# print(values)
+		# print(len(values))
+
+	# truncate at number of samples
+	values = values[:samples]
 
 	# frequency plot
 	plt.figure()
+	plt.title(name)
 	plt.hist(values, 256)
 	plt.savefig(IMG_DIR + name + '_freq.png', bbox_inches='tight')
 
 	# byte heatmap
 	plt.figure()
+	plt.title(name)
 	heatgrid = np.reshape(values, (100, 100))
 	plt.imshow(heatgrid)
 	plt.savefig(IMG_DIR + name + '_bytegrid.png', bbox_inches='tight')
 
-	# print(values[0][0])
-
+	# make equivalent bit array from byte array
 	values_bits = list()
 
 	for val in values:
 		for bit in '{:08b}'.format(val): # convert byte to bits
 			values_bits.append(int(bit))
 
-	print(values_bits)
-
+	# bit heatmap
 	plt.figure()
+	plt.title(name)
 	heatgrid_bits = np.reshape(values_bits, (320,250))
 	plt.imshow(heatgrid_bits)
 	plt.savefig(IMG_DIR + name + '_bitgrid.png', bbox_inches='tight')
-	
-
 
 # Linux's /dev/urandom true RNG
 test_rng('urandom', 10000, lambda: os.urandom(20))
